908e3ed59f761b63e6624f0b2d03f78161e59c14
Yaroslav Kratt <kratt202@gmail.com>
2019-08-09 19:21:05 +0300

    Initial commit

---
+++ src/main/java/ua/kpi/ipt/srom/srom_check/LongArithmeticController.java
@@ -0,0 +1,18 @@
+package ua.kpi.ipt.srom.srom_check;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LongArithmeticController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "index";
+    }
+}
---
+++ src/main/java/ua/kpi/ipt/srom/srom_check/SromCheckApplication.java
@@ -0,0 +1,11 @@
+package ua.kpi.ipt.srom.srom_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SromCheckApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SromCheckApplication.class, args);
+    }
+}
