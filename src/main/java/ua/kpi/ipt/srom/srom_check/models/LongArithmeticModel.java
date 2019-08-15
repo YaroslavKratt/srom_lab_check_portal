3db62b2336921a6470607a95beb159c84eb69c47
Yaroslav Kratt <kratt202@gmail.com>
2019-08-15 20:32:35 +0300

    Hot fix

---
+++ src/main/java/ua/kpi/ipt/srom/srom_check/models/LongArithmeticModel.java
@@ -0,0 +1,32 @@
+package ua.kpi.ipt.srom.srom_check.models;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Setter
+@Getter
+@Builder
+public class LongArithmeticModel {
+    @NotEmpty(message = "{error.empty.field}")
+    private String firstNumber;
+    @NotEmpty(message = "{error.empty.field}")
+    private String secondNumber;
+    private String module;
+    @NotNull
+    private String numberSystem;
+
+    public LongArithmeticModel(String firstNumber, String secondNumber, String module, String numberSystem) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+        this.module = module;
+        this.numberSystem = numberSystem;
+    }
+
+    public LongArithmeticModel() {
+
+    }
+}
