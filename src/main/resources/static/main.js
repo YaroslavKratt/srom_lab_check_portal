938dad37ae974dafe1a22bd33d174acf540ef8b0
YaroslavKratt <wtf0100@gmail.com>
2020-01-23 15:49:15 +0200

    Update styles, add spinners

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -2,3 +2,13 @@ function removeValidation() {
     $(".is-invalid").removeClass("is-invalid");
     $(".invalid-feedback").hide()
 }
+
+$("#confirmCalculationButton").click(function () {
+    if(calculated){
+        spinner($("#cards"));
+    }
+    else {
+        $("#confirmCalculationButton").hide();
+        spinner($(".button-wrapper"))
+    }
+});
